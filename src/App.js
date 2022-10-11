3128445472b23e2ac13e3a2741616e8ecaecbc2b
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-10-10 23:30:24 -0300

    Adicionado usuarios e configuracao de navegacao

--- src/App.js
+++ src/App.js
@@ -3,16 +3,47 @@ import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import UserList from "./views/UserList";
+import UserForm from "./views/UserForm";
+import { Icon, Button } from "@rneui/base";
 
 const Stack = createNativeStackNavigator()
 
 export default props => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="UserList" component={UserList}/>
-                <Stack.Screen name="UserForm" component={UserForm}/>
+            <Stack.Navigator initialRouteName="UserList" 
+            screenOptions={screenOption}>
+                <Stack.Screen name="UserList" 
+                    component={UserList} 
+                    options={({navigation}) => {
+                    return {
+                        title: 'Lista de usuario',
+                        headerRight: () => (
+                            <Button onPress={() => navigation.navigate("UserForm")}
+                                type="clear" 
+                                icon={<Icon name="add"
+                                    size={25}
+                                    color="white"
+                                    />
+                                }
+                            />
+                        )
+                    }
+                }}/>
+                <Stack.Screen name="UserForm" component={UserForm} options={{
+                    title: "Formulario de Usuarios"
+                }}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
+}
+
+const screenOption = {
+    headerStyle: {
+        backgroundColor: '#f4511e'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
 }
---
+++ src/data/users.js
@@ -0,0 +1,28 @@
+const usuarios = [
+    {
+        id: 1,
+        name: "Marcella Campos",
+        email: "jsilva@gmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png"
+    },
+    {
+        id: 2,
+        name: "Guilherme Torres",
+        email: "jsilva@gmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635450_960_720.png"
+    },
+    {
+        id: 3,
+        name: "Bernardo Lopes",
+        email: "jsilva@gmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/19/57/avatar-1295404_960_720.png"
+    },
+    {
+        id: 4,
+        name: "Alessandra Aguiar",
+        email: "jsilva@gmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png"
+    },
+]
+
+export default usuarios;
